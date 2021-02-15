e596e27622a0825d90fd5b7b270bdab8061e0b83
iammiori <mioriii@naver.com>
2021-02-15 15:26:30 +0900

    [feat] init the project

---
+++ MakeUs6th/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakeUs6th
+//
+//  Created by miori Lee on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
