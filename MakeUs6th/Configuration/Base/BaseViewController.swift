b048b32cc4dd917e0b46fee5f397eb045735918f
iammiori <mioriii@naver.com>
2021-02-20 23:35:07 +0900

    [feat] config - base view controller 설정

---
+++ MakeUs6th/Configuration/Base/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  MakeUs6th
+//
+//  Created by miori Lee on 2021/02/20.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Navigation Bar
+        
+        
+        // Background Color
+        self.view.backgroundColor = .white
+    }
+}
