ee6f896b2d26791d849cd57c5d3ffd8dce7f2647
iammiori <mioriii@naver.com>
2021-03-04 21:21:57 +0900

    [feat] configuration 설정

---
+++ MakeUs6th/Configuration/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  MakeUs6th
+//
+//  Created by miori Lee on 2021/03/04.
+//
+
+import Alamofire
+
+struct Constant {
+    static let BASE_URL = "URL 주소를 입력해주세요"
+}
---
+++ MakeUs6th/Configuration/Extension/UINavigationBar.swift
@@ -0,0 +1,23 @@
+//
+//  UINavigationBar.swift
+//  MakeUs6th
+//
+//  Created by miori Lee on 2021/03/04.
+//
+
+import UIKit
+
+extension UINavigationBar {
+    // MARK: 네비게이션바를 투명하게 처리
+    var isTransparent: Bool {
+        get {
+            return false
+        } set {
+            self.isTranslucent = newValue
+            self.setBackgroundImage(newValue ? UIImage() : nil, for: .default)
+            self.shadowImage = newValue ? UIImage() : nil
+            self.backgroundColor = newValue ? .clear : nil
+        }
+    }
+}
+
