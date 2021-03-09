b7e26c5f8ceba31f640f2d63d0e6312a67bedf39
iammiori <mioriii@naver.com>
2021-03-10 01:57:10 +0900

    [feat] base Tab Bar programmatically

--- MakeUs6th/Configuration/Base/BaseTabBarController.swift
+++ MakeUs6th/Configuration/Base/BaseTabBarController.swift
@@ -8,19 +8,21 @@
 import UIKit
 
 class BaseTabBarController: UITabBarController, UITabBarControllerDelegate {
-    let homeViewController  = ViewController()
+    
+    
+    let homeViewController  = HomeViewController()
     let searchViewController  = ViewController()
     let uploadViewController  = ViewController()
     let heartViewController  = ViewController()
     let settingViewController  = ViewController()
-  
     
-    let homeTabBarItem  = UITabBarItem(title: "홈", image: UIImage(systemName: "home"), tag: 0)
-    let searchTabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "search"), tag: 1)
-    let uploadTabBarItem  = UITabBarItem(title: "업로드", image: UIImage(systemName: "upload"), tag: 2)
-    let heartTabBarItem = UITabBarItem(title: "찜목록", image: UIImage(systemName: "heart"), tag: 3)
-    let settingTabBarItem  = UITabBarItem(title: "설정", image: UIImage(systemName: "user"), tag: 4)
-
+    
+    let homeTabBarItem  = UITabBarItem(title: "홈", image: UIImage(systemName: "house"), tag: 0)
+    let searchTabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+    let uploadTabBarItem  = UITabBarItem(title: "업로드", image: UIImage(systemName: "plus.circle"), tag: 2)
+    let heartTabBarItem = UITabBarItem(title: "찜목록", image: UIImage(systemName: "heart") , tag: 3)
+    let settingTabBarItem  = UITabBarItem(title: "설정", image: UIImage(systemName: "person"), tag: 4)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,5 +43,32 @@ class BaseTabBarController: UITabBarController, UITabBarControllerDelegate {
         self.viewControllers = [homeNavigationController, searchNavigationController,uploadNavigationController,heartNavigationController,settingNavigationController]
         
         self.delegate = self
+        
+        // tabBar color 변경
+        setAppearance()
+        
+        
+    }
+    // tabBar color 변경 func
+    func setAppearance(){
+        if #available(iOS 13, *) {
+            let appearance = UITabBarAppearance()
+            
+            appearance.backgroundColor = .white
+            appearance.shadowImage = UIImage()
+            appearance.shadowColor = .white
+            
+            appearance.stackedLayoutAppearance.normal.iconColor = .tabUnSelected
+            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.tabUnSelected]
+            //appearance.stackedLayoutAppearance.normal.badgeBackgroundColor = .blue
+            
+            appearance.stackedLayoutAppearance.selected.iconColor = .black
+            appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+            
+            self.tabBar.standardAppearance = appearance
+            
+        }
     }
 }
+
+
