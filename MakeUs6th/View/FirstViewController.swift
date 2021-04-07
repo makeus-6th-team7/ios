32af180c226dd36c25962c5fa1786ef43643826f
iammiori <mioriii@naver.com>
2021-04-07 22:12:53 +0900

    [feat] first view image 추가

--- MakeUs6th/View/FirstViewController.swift
+++ MakeUs6th/View/FirstViewController.swift
@@ -33,13 +33,24 @@ class FirstViewController: UIViewController {
         $0.textColor = .black
         $0.font = UIFont.systemFont(ofSize: 26)
     }
+    
+    let subLabel = UILabel().then {
+        $0.text = "심사숙소로 숙소 후기를 한 번에 확인해보세요."
+        $0.textColor = .bwg7
+        $0.font = UIFont.systemFont(ofSize: 14)
+    }
+    
+    let imgView = UIImageView().then {
+        $0.image = (#imageLiteral(resourceName: "main"))
+        $0.contentMode = .scaleAspectFit
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
-        _ = [mainLabel,underlineView,lastLabel].map { self.view.addSubview($0)}
+        _ = [mainLabel,underlineView,lastLabel,subLabel,imgView].map { self.view.addSubview($0)}
         _ = [appNameLabel].map {self.underlineView.addSubview($0)}
         bindConstraints()
     }
@@ -69,6 +80,17 @@ extension FirstViewController {
         lastLabel.snp.makeConstraints {
             $0.top.equalTo(appNameLabel)
             $0.leading.equalTo(appNameLabel.snp.trailing).offset(5)
+        }
+        
+        subLabel.snp.makeConstraints {
+            $0.top.equalTo(underlineView.snp.bottom).offset(18)
+            $0.leading.equalTo(mainLabel)
+        }
+        
+        imgView.snp.makeConstraints {
+            $0.top.equalTo(subLabel.snp.bottom).offset(35)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(394)
         }
     }
 }
