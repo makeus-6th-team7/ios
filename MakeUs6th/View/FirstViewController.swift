312ef8eca4573242dfe3a95ec9f36ba69422c768
iammiori <mioriii@naver.com>
2021-04-09 23:36:59 +0900

    [feat] kakao, apple login view 추가

--- MakeUs6th/View/FirstViewController.swift
+++ MakeUs6th/View/FirstViewController.swift
@@ -42,16 +42,51 @@ class FirstViewController: UIViewController {
     
     let imgView = UIImageView().then {
         $0.image = (#imageLiteral(resourceName: "main"))
-        $0.contentMode = .scaleAspectFit
+        $0.contentMode = .scaleToFill
+        //$0.clipsToBounds = false
     }
+    
+    let divisonView = UIView().then {
+        $0.backgroundColor = .bwg2
+    }
+    
+    let signUpLabel = UILabel().then {
+        $0.text = "쉽고 빠른 회원가입!"
+        $0.font = UIFont.systemFont(ofSize: 11)
+        $0.textColor = .bwg7
+    }
+    
+    let kakaoCircle = UIView().then {
+        $0.backgroundColor = UIColor(red: 0.996, green: 0.898, blue: 0, alpha: 1)
+        $0.cornerRadius = 22
+    }
+    
+    let kakaoIV = UIImageView().then {
+        $0.image = #imageLiteral(resourceName: "kakao")
+        $0.contentMode = .scaleToFill
+    }
+    
+    let appleCircle = UIView().then {
+        $0.backgroundColor = .black
+        $0.cornerRadius = 22
+    }
+    
+    let appleIV = UIImageView().then {
+        $0.image = #imageLiteral(resourceName: "apple")
+        $0.contentMode = .scaleToFill
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
-        _ = [mainLabel,underlineView,lastLabel,subLabel,imgView].map { self.view.addSubview($0)}
+        _ = [mainLabel,underlineView,lastLabel,subLabel,imgView,divisonView,signUpLabel,kakaoCircle,appleCircle].map { self.view.addSubview($0)}
         _ = [appNameLabel].map {self.underlineView.addSubview($0)}
+        _ = [kakaoIV].map {self.kakaoCircle.addSubview($0)}
+        _ = [appleIV].map {self.appleCircle.addSubview($0)}
         bindConstraints()
     }
 
@@ -90,7 +125,42 @@ extension FirstViewController {
         imgView.snp.makeConstraints {
             $0.top.equalTo(subLabel.snp.bottom).offset(35)
             $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(394)
+            $0.bottom.equalTo(divisonView.snp.top).offset(-30)
+        }
+        
+        divisonView.snp.makeConstraints {
+            $0.bottom.equalTo(view.snp.bottom).offset(-141)
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        signUpLabel.snp.makeConstraints {
+            $0.top.equalTo(divisonView.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+        }
+        
+        kakaoCircle.snp.makeConstraints {
+            $0.top.equalTo(signUpLabel.snp.bottom).offset(24)
+            $0.leading.equalToSuperview().offset(115)
+            $0.width.equalTo(44)
+            $0.height.equalTo(44)
+        }
+        
+        kakaoIV.snp.makeConstraints {
+            $0.top.equalTo(kakaoCircle.snp.top).offset(12)
+            $0.leading.equalTo(kakaoCircle.snp.leading).offset(10)
+        }
+        
+        appleCircle.snp.makeConstraints {
+            $0.top.equalTo(signUpLabel.snp.bottom).offset(24)
+            $0.trailing.equalToSuperview().offset(-115)
+            $0.width.equalTo(44)
+            $0.height.equalTo(44)
+        }
+        
+        appleIV.snp.makeConstraints {
+            $0.top.equalTo(appleCircle.snp.top).offset(11)
+            $0.leading.equalTo(appleCircle.snp.leading).offset(14)
         }
     }
 }
